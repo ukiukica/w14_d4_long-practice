465f157ada01b1379f331fb9b38a9734c764c5ea
uki <ukiworks034@gmail.com>
2022-05-19 18:44:04 -0400

    completed

--- Second Long Pract with Tests/src/components/SetCoffeeBean.js
+++ Second Long Pract with Tests/src/components/SetCoffeeBean.js
@@ -1,8 +1,22 @@
+import {useEffect, useState, useContext} from 'react';
+import { CoffeeContext } from '../context/CoffeeContext';
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+  const {coffeeBean, setCoffeeBeanId} = useContext(CoffeeContext)
+  const [selectedBean, setSelectedBean] = useState(coffeeBean.id)
+
+  useEffect(() => {
+    // console.log(selectedBean)
+    setSelectedBean(selectedBean)
+  }, [coffeeBean, setCoffeeBeanId, selectedBean])
+
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
+        onChange={e => setCoffeeBeanId(e.target.value)}
+
+        value={selectedBean}
         name="coffee-bean"
       >
         {coffeeBeans.map(bean => (
