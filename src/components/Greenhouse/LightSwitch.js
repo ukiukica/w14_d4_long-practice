f25270fd19d3e7c3cb8e133f5d1bb110bf46b273
uki <ukiworks034@gmail.com>
2022-05-19 16:00:19 -0400

    completed phase 2

--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,11 +1,17 @@
 import './LightSwitch.css';
+import { useContext } from 'react';
+import { useTheme } from '../../context/ThemeContext'
 
 function LightSwitch() {
+  const {themeName, setThemeName} = useTheme();
+  // let dayBool = true;
+  // themeName === 'day' ? dayBool = true : dayBool = false;
+
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
-    </div>
+    <div className={`light-switch ${themeName}`}>
+      <div className="on" onClick={() => setThemeName('day')}>DAY</div>
+      <div className="off" onClick={() => setThemeName('night')}>NIGHT</div>
+      </div>
   );
 }
 
