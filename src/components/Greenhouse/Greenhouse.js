a4e4b21335a866dcf58ae24ce51e9c400a8e01be
Andrew Stilinovic <stili87@gmail.com>
2022-05-19 13:02:29 -0500

    finished phase 1

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,16 +1,27 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
-
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
+import {useTheme} from '../../context/ThemeContext'
+import {useContext} from 'react'
 
 function Greenhouse() {
+const {themeName, setThemeName} = useTheme()
+let srcImg = ''
 
+// if(themeName === 'day'){
+//   srcImg = dayImage
+// }else {
+//   srcImg = nightImage
+// }
+themeName === 'day' ? srcImg = dayImage : srcImg = nightImage
+
+console.log(srcImg)
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
+            src={srcImg}
             alt='greenhouse' 
       />
       <LightSwitch />
